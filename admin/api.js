eb8a6cffb07044bb4c6bdae44aeb1235462089b9
zhihyan <97696234+zhihyan@users.noreply.github.com>
2022-08-02 13:52:37 +0800

    Merged functions of 3.1 3.2 3.3

--- admin/api.js
+++ admin/api.js
@@ -6,7 +6,6 @@ const {Fund} = require('./DbConfig.js');
 const {Contributor} = require('./DbConfig.js');
 const {Donation} = require('./DbConfig.js');
 
-
 /*
 create a new organization
 */
@@ -397,15 +396,138 @@ app.use('/allOrgs', (req, res) => {
 	    }).sort({ 'name': 'asc' });
     });
 
+/**
+ *  find a contributor by login ID
+ */
+app.use("/findContributorByLogin", (req, res)=>{
+	const query = {"login" : req.query.login};
+    
+	Contributor.findOne( query, (err, result) => {
+		if (err) {
+		    res.json({ "status": "error", "data" : err});
+		}
+		else if (!result){
+		    res.json({ "status": "not found", 'data': 'this login id has not been taken yet' });
+		}
+		else {
+		    res.json({ "status" : "success", "data" : result});
+		}
+	    });
+});
 
+/**
+ * create a new contributor
+ */
+app.use("/createContributor", (req, res)=>{
+	const contributor = new Contributor({
+		login: req.query.login,
+		password: req.query.password,
+		name: req.query.name,
+		email: req.query.email,
+		creditCardNumber: req.query.creditCardNumber,
+		creditCardCVV: req.query.creditCardCVV,
+		creditCardExpiryMonth: req.query.creditCardExpiryMonth,
+		creditCardExpiryYear: req.query.creditCardExpiryYear,
+		creditCardPostCode: req.query.creditCardPostCode,
+		donations: []
+	});
+
+	contributor.save((err)=>{
+		if (err) {
+			res.json({'status': 'error', 'data': err});
+		} else {
+			res.json({'status': 'success', 'data': "Successfully created a new contributor"});
+		}
+	});
+});
 
 /********************************************************/
-app.use('/', (req, res) => {
-	res.write("<a href=\"/allOrgs\">Administer Organizations</a>");
-	res.write("<p>");
-	res.write("<a href=\"/allContributors\">Administer Contributors</a>");
-	res.send();
-    });
+
+/**
+ * Vertify organization’s password
+ */
+ app.use('/vertifyOrgPasswd', (req, res) => {
+
+	var filter = {"_id" : req.query.id };
+	
+
+	Organization.findOne( filter, (err, result) => {
+		if (err) {
+		    res.json({'status': 'error', 'data': err});
+		} else if (result.password != req.query.password) {
+		    res.json({'status': 'error', 'data': "password is not equal"});
+		} else {
+			res.json({'status': 'success'});
+		}
+	});
+});
+
+/**
+ * Change organization’s password
+ */
+app.use('/updateOrgPassword', (req, res) => {
+
+	var filter = {"_id" : req.query.id };
+
+	var update = { "password" : req.query.password};
+	
+	var action = { "$set" : update };
+	
+
+	Organization.findOne( filter, (err, result) => {
+		if (err) {
+		    res.json({'status': 'error', 'data': err});
+		} else if (result.password != req.query.oldpassword) {
+		    res.json({'status': 'error', 'data': "Old password is not equal"});
+		} else {
+			Organization.findOneAndUpdate( filter, action, { new : true }, (err, result) => {
+				if (err) {
+					res.json({'status': 'error', 'data': err});
+				} else {
+					
+					res.json({'status': 'success', 'data': "Successfully updated Organization's password"});
+					
+				}
+			});
+		}
+	});
+	
+	
+});
+
+
+/**
+ * Update organization’s account information
+ */
+app.use('/updateOrgInfo', (req, res) => {
+
+	var filter = {"_id" : req.query.id };
+
+	var update = { "name" : req.query.name, "description" : req.query.description};
+	
+	var action = { "$set" : update };
+	
+
+	Organization.findOne( filter, (err, result) => {
+		if (err) {
+		    res.json({'status': 'error', 'data': err});
+		} else if (result.password != req.query.password) {
+		    res.json({'status': 'error', 'data': "password is not equal"});
+		} else {
+			Organization.findOneAndUpdate( filter, action, { new : true }, (err, result) => {
+				if (err) {
+					res.json({'status': 'error', 'data': err});
+				} else {
+					
+					res.json({'status': 'success', 'data': "Successfully updated Organization's information"});
+					
+				}
+			});
+		}
+	});
+	
+	
+});
 
 app.listen(3001,  () => {
 	console.log('Listening on port 3001');
